605c5c74539999cc221f02fe8fffc3c02cd9d430
black-code1 <legrandmahamat1@gmail.com>
2021-07-20 04:09:14 +0100

    How to Link to Different Pages

---
+++ resources/views/post.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+
+  <article>
+    <h1><a href="/post">My First Post</a></h1>
+
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor, repudiandae iusto ut vitae neque sint
+      maiores ullam nobis dolores, reprehenderit molestias ducimus adipisci aliquam error! Earum quod voluptatibus
+      provident fugit?
+    </p>
+
+  </article>
+
+  <a href="/">Go Back</a>
+</body>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor, repudiandae iusto ut vitae neque sint
+            maiores ullam nobis dolores, reprehenderit molestias ducimus adipisci aliquam error! Earum quod voluptatibus
+            provident fugit?
+        </p>
+
+    </article>
+
+    <article>
+        <h1><a href="/post">My Second Post</a></h1>
+
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor, repudiandae iusto ut vitae neque sint
+            maiores ullam nobis dolores, reprehenderit molestias ducimus adipisci aliquam error! Earum quod voluptatibus
+            provident fugit?
+        </p>
+
+    </article>
+
+    <article>
+        <h1><a href="/post">My Third Post</a></h1>
+
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor, repudiandae iusto ut vitae neque sint
+            maiores ullam nobis dolores, reprehenderit molestias ducimus adipisci aliquam error! Earum quod voluptatibus
+            provident fugit?
+        </p>
+
+    </article>
+</body>
