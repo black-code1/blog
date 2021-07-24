76d77f1b3351d8c0081a22af43e596fe9ad1b662
black-code1 <legrandmahamat1@gmail.com>
2021-07-24 20:40:11 +0100

    Read all Posts with the Filesystem class

---
+++ resources/posts/my-fourth-post.blade.php
@@ -0,0 +1,14 @@
+---
+
+title: My Fourth Post
+excerpt: Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+date: 2021-05-21
+
+---
+
+<p>
+  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor, repudiandae
+  iusto ut vitae neque sint maiores ullam nobis dolores, reprehenderit
+  molestias ducimus adipisci aliquam error! Earum quod voluptatibus provident
+  fugit?
+</p>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,37 +4,9 @@
 <link rel="stylesheet" href="/app.css">
 
 <body>
-
+    <?php foreach ($posts as $post) : ?>
     <article>
-        <h1><a href="/posts/my-first-post">My First Post</a></h1>
-
-        <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor, repudiandae iusto ut vitae neque sint
-            maiores ullam nobis dolores, reprehenderit molestias ducimus adipisci aliquam error! Earum quod voluptatibus
-            provident fugit?
-        </p>
-
+        <?= $post; ?>
     </article>
-
-    <article>
-        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
-
-        <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor, repudiandae iusto ut vitae neque sint
-            maiores ullam nobis dolores, reprehenderit molestias ducimus adipisci aliquam error! Earum quod voluptatibus
-            provident fugit?
-        </p>
-
-    </article>
-
-    <article>
-        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
-
-        <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor, repudiandae iusto ut vitae neque sint
-            maiores ullam nobis dolores, reprehenderit molestias ducimus adipisci aliquam error! Earum quod voluptatibus
-            provident fugit?
-        </p>
-
-    </article>
+    <?php endforeach; ?>
 </body>
