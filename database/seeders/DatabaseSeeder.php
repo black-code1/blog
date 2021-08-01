194805ca7d1e1ce2cc38e8c186c876df310b8b91
black-code1 <legrandmahamat1@gmail.com>
2021-08-01 12:13:44 +0100

    Turbo Boost With Model Factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,43 +16,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
-
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
+        $user = User::factory()->create([
+            'name' => 'John Doe'
         ]);
-
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family'
-        ]);
-
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $family->id,
-            'title' => 'My Family Post',
-            'slug' => 'my-first-post',
-            'excerpt' => '<p>Lorem ipsum dolar sit amet.</p>',
-            'body' => '<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis labore molestias quis odio velit unde est excepturi distinctio eius, expedita ad ipsam nulla! Maxime quisquam explicabo officiis consequuntur voluptates fugiat exercitationem, debitis vero! Minus quod, ipsum quibusdam eveniet similique odit?</p>'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My Work Post',
-            'slug' => 'my-work-post',
-            'excerpt' => '<p>Lorem ipsum dolar sit amet.</p>',
-            'body' => '<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis labore molestias quis odio velit unde est excepturi distinctio eius, expedita ad ipsam nulla! Maxime quisquam explicabo officiis consequuntur voluptates fugiat exercitationem, debitis vero! Minus quod, ipsum quibusdam eveniet similique odit?</p>'
+        Post::factory(5)->create([
+            'user_id' => $user->id
         ]);
     }
 }
