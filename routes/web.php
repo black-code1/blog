f3e62bfccd5f915743b13109bcce2de8a93040fa
black-code1 <legrandmahamat1@gmail.com>
2021-08-08 17:57:42 +0100

    Extract a reusable deopdown blade component

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/', function () {
         'categories' => Category::all()
 
     ]);
-});
+})->name('home');
 
 Route::get('posts/{post:slug}', function (Post $post) {
     // Find a post by its slug and pass it to a view called "post"
@@ -33,7 +33,7 @@ Route::get('posts/{post:slug}', function (Post $post) {
 
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', ['posts' => $category->posts, 'currentCategory' => $category, 'categories' => Category::all()]);
-});
+})->name('category');
 
 Route::get('authors/{author:username}', function (User $author) {
 
